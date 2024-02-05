5ec09f6fada79d1aa6ab7c8f4a409fb5488f458d
Speuce <mrk5199@gmail.com>
2024-02-04 18:29:50 -0600

    Button Increment Feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,31 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    axios.get('/getCounter')
+      .then(res => {
+        setCounter(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  const incrementCounter = () => {
+    axios.post('/incrementCounter')
+      .then(res => {
+        setCounter(res.data);
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Counter: {counter}</p>
+        <button onClick={incrementCounter}>Increment Counter</button>
       </header>
     </div>
   );
---
+++ server/counter.js
@@ -0,0 +1,14 @@
+let counter = 0;
+
+const incrementCounter = () => {
+  counter++;
+};
+
+const getCounter = () => {
+  return counter;
+};
+
+module.exports = {
+  incrementCounter,
+  getCounter
+};
