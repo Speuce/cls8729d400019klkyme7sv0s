d4ede411d608ddc95e92939d92b79acff4e163df
Speuce <mrk5199@gmail.com>
2024-02-04 18:30:20 -0600

    Frontend Counter Display

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import axios from 'axios';
-
-function App() {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    axios.get('/getCounter')
-      .then(res => {
-        setCounter(res.data);
-      })
-      .catch(err => console.log(err));
-  }, []);
-
-  const incrementCounter = () => {
-    axios.post('/incrementCounter')
-      .then(res => {
-        setCounter(res.data);
-      })
-      .catch(err => console.log(err));
-  };
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Counter: {counter}</p>
-        <button onClick={incrementCounter}>Increment Counter</button>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+ useEffect(() => {
+   axios.get('/getCounter')
+     .then(res => {
+       setCounter(res.data);
+     })
+     .catch(err => console.log(err));
+ }, []);
